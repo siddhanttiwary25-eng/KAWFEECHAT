f47306777ce3850385901e10c45d73efc7ed25d1
siddhanttiwary25-eng <siddhanttiwary25@gmail.com>
2026-06-15 22:12:18 -0700

    Delete api directory

--- api/api/health.js
+++
@@ -1,5 +0,0 @@
-// api/health.js
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({ status: 'ok', message: 'Backend is running.' });
-}
